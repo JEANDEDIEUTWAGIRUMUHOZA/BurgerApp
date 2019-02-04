848854dda25c96a008015cb34a16368085d93142
jado <jeandedieu.twagirumuhoza@gmail.com>
2019-02-04 11:11:04 +0100

    added app module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BurgersComponent} from './burgers/burgers.component';
+import {BurgerComponent} from './burger/burger.component';
+import {AddBurgerComponent} from './add-burger/add-burger.component';
+import {AddVeggieComponent} from './add-veggie/add-veggie.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
+import { MenuComponent } from './menu/menu.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgersComponent,
+    BurgerComponent,
+    AddBurgerComponent,
+    AddVeggieComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
